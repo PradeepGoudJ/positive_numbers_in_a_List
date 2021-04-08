96cfc8aa247d463040895ff05295166908becae6
PradeepGoudJ <82086204+PradeepGoudJ@users.noreply.github.com>
2021-04-08 21:29:09 +0530

    Add files via upload

---
+++ liist.py
@@ -0,0 +1,9 @@
+List1=[11,-21,0,45,66,-93]
+List2=[12,14,-95,3]
+for num in List1:
+    if num >= 0:
+        print(num, end="  ")
+print("\n")        
+for num in List2:
+    if num >= 0 :
+        print(num, end="  ")
